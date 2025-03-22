2e0d139f0e9573cdc23b7586da596306dbed0718
DamaMichaelYohanna <get2dama11@gmail.com>
2025-03-22 06:56:49 +0100

    core/updated url

--- label_x/urls.py
+++ label_x/urls.py
@@ -28,5 +28,5 @@ urlpatterns = [
     # Redoc UI (alternative API docs)
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('api/v1/account/', include("account.urls")),
-    path('api/v1/tasks', include('task.urls')),
+    path('api/v1/tasks/', include('task.urls')),
 ]
