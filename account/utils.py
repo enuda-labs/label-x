0ef64dac258e403bdf7b08e593db76bc5926be9e
DamaMichaelYohanna <get2dama11@gmail.com>
2025-04-11 15:45:14 +0100

    core/added custom permission

---
+++ account/utils.py
@@ -0,0 +1,14 @@
+from rest_framework.permissions import BasePermission
+
+class IsAdminUser(BasePermission):
+    """Allow access to only users mark as admin"""
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated and request.user.is_admin
+    
+
+class IsReviewer(BasePermission):
+    """
+    Allows access only to users marked as reviewers.
+    """
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated and request.user.is_reviewer)
