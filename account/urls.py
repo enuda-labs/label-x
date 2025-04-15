681c8bf23119de0e1eda1ff20589b223183a23c1
DamaMichaelYohanna <get2dama11@gmail.com>
2025-04-15 09:14:25 +0100

    added urls for new endpoints

--- account/urls.py
+++ account/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
-from .apis import LoginView, RegisterView, CustomTokenRefreshView, MakeUserReviewerView, CreateProjectView, MakeUserAdminView, ProjectListView, UserDetailView, UsersNotInProjectView
+from . import apis
 app_name = 'account'
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='login'),
-    path('register/', RegisterView.as_view(), name='register'),
-    path('make-reviewer/', MakeUserReviewerView().as_view(), name='create-reviewer'),
-    path('user/detail/', UserDetailView.as_view(), name='user-detail'),
-    path('make-admin/', MakeUserAdminView().as_view(), name='create-reviewer'),
-    path('token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
-    path('projects/create/', CreateProjectView.as_view(), name='create-project'),
-    path('projects/list/', ProjectListView.as_view(), name='list -project'),
-    path('users/not-in-project/<int:project_id>/', UsersNotInProjectView.as_view(), name='users-not-in-project'),
+    path('login/', apis.LoginView.as_view(), name='login'),
+    path('register/', apis.RegisterView.as_view(), name='register'),
+    path('make-reviewer/', apis.MakeUserReviewerView().as_view(), name='create-reviewer'),
+    path('remove-reviewer/', apis.RemoveUserReviewerView().as_view(), name='create-reviewer'),
+    path('user/detail/', apis.UserDetailView.as_view(), name='user-detail'),
+    path('make-admin/', apis.MakeUserAdminView().as_view(), name='create-reviewer'),
+    path('token/refresh/', apis.CustomTokenRefreshView.as_view(), name='token_refresh'),
+    path('projects/create/', apis.CreateProjectView.as_view(), name='create-project'),
+    path('projects/list/', apis.ProjectListView.as_view(), name='list -project'),
+    path('users/not-in-project/<int:project_id>/', apis.UsersNotInProjectView.as_view(), name='users-not-in-project'),
+    path('users/in-project/<int:project_id>/', apis.UsersInProjectView.as_view(), name='users-in-project'),
 ]
