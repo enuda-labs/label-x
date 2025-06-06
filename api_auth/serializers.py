20a973f3c74402febc92d94ae059bca8d369d7cd
Code bee <codebee345@outlook.com>
2025-06-06 06:52:34 -0400

    allow users to enter the api key names themselves

--- api_auth/serializers.py
+++ api_auth/serializers.py
@@ -3,6 +3,15 @@ from django.contrib.auth import authenticate
 from account.models import UserAPIKey
 
 
+class GenerateApiKeySerializer(serializers.Serializer):
+    key_name = serializers.CharField()
+    
+    def validate_key_name(self, value):
+        if UserAPIKey.objects.filter(name=value, revoked=False):
+            raise serializers.ValidationError("You already have an active api key with this name")
+        return value
+
+
 class APIKeySerializer(serializers.ModelSerializer):
     class Meta:
         model = UserAPIKey
