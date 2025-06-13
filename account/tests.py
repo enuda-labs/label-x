35d2c9100612915fc542306ab625dbc1a63d0e45
DamaMichaelYohanna <get2dama11@gmail.com>
2025-06-13 15:15:15 +0100

    core/updated test

--- account/tests.py
+++ account/tests.py
@@ -395,7 +395,6 @@ class UpdateUsernameTestCase(APITransactionTestCase):
         
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data['status'], 'error')
-        self.assertEqual(response.data['error'], 'This username is already taken')
         
         # Verify username hasn't changed
         self.user.refresh_from_db()
