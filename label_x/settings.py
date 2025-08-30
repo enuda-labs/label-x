a6c0ceff73b69a610c6f202856d4e527c13f3e87
Code bee <codebee345@outlook.com>
2025-08-30 10:27:18 +0100

    fix revert debug

--- label_x/settings.py
+++ label_x/settings.py
@@ -131,7 +131,7 @@ ASGI_APPLICATION = "label_x.asgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-if not DEBUG:
+if DEBUG:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
