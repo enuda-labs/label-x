091540b666c6fe99485dadacf43521c85ae82fa2
DamaMichaelYohanna <get2dama11@gmail.com>
2025-09-17 01:45:24 +0100

    core/register model to admin site

--- common/admin.py
+++ common/admin.py
@@ -1,3 +1,9 @@
+# app/admin.py
 from django.contrib import admin
+from .models import SystemSetting
 
-# Register your models here.
+@admin.register(SystemSetting)
+class SystemSettingAdmin(admin.ModelAdmin):
+    list_display = ("key", "value")
+    search_fields = ("key",)
+
