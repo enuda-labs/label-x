9081c1189adec5921fddf3565e8418575a7bc157
Code bee <codebee345@outlook.com>
2025-09-22 03:50:36 -0400

    changes

--- account/serializers.py
+++ account/serializers.py
@@ -128,7 +128,7 @@ class ProjectDetailSerializer(serializers.ModelSerializer):
         #     (completed_tasks / total_tasks * 100) if total_tasks > 0 else 0
         # )
         
-        completion_percentage = obj.get_cluster_label_completion_percentage() or 0
+        completion_percentage = obj.get_cluster_label_completion_percentage()
         return {
             "completion_percentage": round(completion_percentage, 2),
             "total_used_data_points": total_data_points.get("data_points", 0) or 0,
