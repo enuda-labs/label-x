127e66a34b68873f6db3db19a94cfd7598bb45bb
Code bee <codebee345@outlook.com>
2025-09-22 08:09:26 -0400

    register transactions in admin page

--- payment/models.py
+++ payment/models.py
@@ -31,10 +31,6 @@ class Transaction(models.Model):
 
 
 class WithdrawalRequest(models.Model):
-    # user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    # usd_amount = models.DecimalField(max_digits=10, decimal_places=2)
-    # ngn_amount = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, help_text="The amount in NGN")
-    # status = models.CharField(max_length=50, default=TransactionStatusChoices.PENDING, choices=TransactionStatusChoices.choices)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     reference = models.CharField(max_length=255)
@@ -45,4 +41,4 @@ class WithdrawalRequest(models.Model):
     transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE, null=True, blank=True, help_text="The transaction that this withdrawal request is associated with")
     
     def __str__(self):
-        return f"Withdrawal request of ${self.usd_amount} by {self.user.username} - {self.status}"
+        return f"Withdrawal request of ${self.transaction.usd_amount} by {self.transaction.username} - {self.transaction.status}"
